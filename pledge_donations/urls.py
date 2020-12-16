71ef4ad96f41e60a2f6e8620734474857246b2f7
Ajawu David <ajawudavid@gmail.com>
2020-12-16 08:37:09 +0100

    added authentication pages

--- pledge_donations/urls.py
+++ pledge_donations/urls.py
@@ -5,4 +5,6 @@ app_name = 'pledge_donations'
 
 urlpatterns = [
     path('', views.LandingPageView.as_view(), name='landing_page'),
+    path('contact', views.LandingPageView.as_view(), name='contact_page'),
+    # path('test', views.TesterView.as_view(), name='tester_page'),
 ]
--- pledge_donations/views.py
+++ pledge_donations/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class LandingPageView(TemplateView):
     template_name = 'pledge_donations/index.html'
+
+
+class TesterView(TemplateView):
+    template_name = 'account/password_reset_done.html'
